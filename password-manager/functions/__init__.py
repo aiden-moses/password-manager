6ae9f5a20579424e8fe4926f604848e41b2da6c4
aiden moses <aidenmoses002@gmail.com>
2024-01-06 14:31:41 -0800

    Login Added

--- password-manager/functions/__init__.py
+++ password-manager/functions/__init__.py
@@ -1,3 +1,4 @@
-from .common.clear import clear_function # noqa: E
-from .login.login_menu import login_menu_function # noqa: E
-from .common.exit import exit_function # noqa: E
+from .common.clear import clear_function # noqa
+from .login.login_menu import login_menu_function # noqa
+from .common.exit import exit_function # noqa
+from .login.register import register_function # noqa
