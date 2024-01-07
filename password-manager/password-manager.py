9b390f01c9b13481da6c5eb6a89e082660555cb4
aiden moses <aidenmoses002@gmail.com>
2024-01-07 14:41:31 -0800

    Cleanup

--- password-manager/password-manager.py
+++ password-manager/password-manager.py
@@ -1,32 +1,23 @@
-# Directory Imports
 import functions
 
-# Main Loop
 while True:
     # Login Loop
     while True:
         selection = functions.login_menu_function()
-        # Login
         if selection == "1":
             username = functions.login_function()
             break
-        # Register
         elif selection == "2":
             functions.register_function()
-        # Exit
         elif selection == "3":
             functions.exit_function()
     # Main Menu Loop
     while True:
-        functions.clear_function()
         selection = functions.main_menu_function()
         cipher = functions.security_init_function(username)
-        # View Logins
         if selection == "1":
             functions.view_logins_function(username, cipher)
-        # New Login
         elif selection == "2":
             functions.new_login_function(cipher, username)
-        # Exit
         elif selection == "3":
             functions.exit_function()
