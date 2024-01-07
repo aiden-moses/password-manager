c762bcc7302d688cdac79c0753890b58db2f0b1c
aiden moses <aidenmoses002@gmail.com>
2024-01-06 18:00:49 -0800

    Fixed table bug.

--- password-manager/functions/menu/view_logins.py
+++ password-manager/functions/menu/view_logins.py
@@ -5,6 +5,7 @@ import functions
 from rich.console import Console
 from rich.table import Table
 console = Console()
+table = None
 table = Table(title="Password Manager - Saved Passwords")
 
 
@@ -18,14 +19,13 @@ def table_function(view, cipher):
         encrypted_password = x['encrypted_password']
         decrypted_password = functions.decrypt_function(cipher, encrypted_password) # noqa
         table.add_row(application, user, decrypted_password)
-    while True:
-        functions.clear_function()
-        console.print(table)
-        selection = input("\nWould you like to return to the main menu?\nY/n>> ") # noqa
-        if selection == "y" or selection == "Y" or not selection:
-            break
-        else:
-            pass
+    functions.clear_function()
+    console.print(table)
+    selection = input("\nWould you like to return to the main menu?\nY/n>> ") # noqa
+    if selection == "y" or selection == "Y" or not selection:
+        return
+    else:
+        pass
 
 
 def view_logins_function(username, cipher):
@@ -36,7 +36,25 @@ def view_logins_function(username, cipher):
     try:
         with open(file_directory, 'r') as data:
             view = json.load(data)
-            table_function(view, cipher)
+            table = Table(title="Password Manager - Saved Passwords")
+            table.add_column("Application / Website", justify='center', no_wrap=True) # noqa
+            table.add_column("Username", justify='center', no_wrap=True)
+            table.add_column("Password", justify='center', no_wrap=True)
+            for x in view:
+                user = x['username']
+                application = x['application']
+                encrypted_password = x['encrypted_password']
+                decrypted_password = functions.decrypt_function(cipher, encrypted_password) # noqa
+                table.add_row(application, user, decrypted_password)
+            functions.clear_function()
+            console.print(table)
+            table = None
+            selection = input("\nWould you like to return to the main menu?\nY/n>> ") # noqa
+            if selection == "y" or selection == "Y" or not selection:
+                return
+            else:
+                pass
+
     except FileNotFoundError:
         functions.clear_function()
         print("Error: You have not saved any passwords.")
